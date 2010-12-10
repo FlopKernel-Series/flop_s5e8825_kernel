beagleboard/linux

usb: musb: drop musb_platform_save/restore_context
... that can be easily folded into the
musb_platform_suspend/resume calls.

Signed-off-by: Felipe Balbi <balbi@ti.com>

drivers/usb/musb/am35x.c
@@ -468,19 +468,19 @@ static int am35x_platform_exit(struct musb *musb)
 	return 0;
 }
 
-#ifdef CONFIG_PM
-void musb_platform_save_context(struct musb *musb,
-	struct musb_context_registers *musb_context)
+static int am35x_suspend(struct musb *musb)
 {
 	phy_off();
+
+	return 0;
 }
 
-void musb_platform_restore_context(struct musb *musb,
-	struct musb_context_registers *musb_context)
+static int am35x_resume(struct musb *musb)
 {
 	phy_on();
+
+	return 0;
 }
-#endif
 
 /* AM35x supports only 32bit read operation */
 void musb_read_fifo(struct musb_hw_ep *hw_ep, u16 len, u8 *dst)
@@ -521,6 +521,9 @@ static struct musb_platform_ops am35x_ops = {
 
 	.set_mode	= am35x_set_mode,
 	.try_idle	= am35x_try_idle,
+
+	.suspend	= am35x_suspend,
+	.resume		= am35x_resume,
 
 	.set_vbus	= am35x_set_vbus,
 };

drivers/usb/musb/blackfin.c
@@ -408,9 +408,7 @@ static int bfin_platform_init(struct musb *musb, void *board_data)
 	return 0;
 }
 
-#ifdef CONFIG_PM
-void musb_platform_save_context(struct musb *musb,
-			struct musb_context_registers *musb_context)
+static int bfin_suspend(struct musb *musb)
 {
 	if (is_host_active(musb))
 		/*
@@ -420,14 +418,16 @@ void musb_platform_save_context(struct musb *musb,
 		 * wakeup event.
 		 */
 		gpio_set_value(musb->config->gpio_vrsel, 0);
+
+	return 0;
 }
 
-void musb_platform_restore_context(struct musb *musb,
-			struct musb_context_registers *musb_context)
+static int bfin_resume(struct musb *musb)
 {
 	bfin_reg_init(musb);
+
+	return 0;
 }
-#endif
 
 static int bfin_platform_exit(struct musb *musb)
 {
@@ -447,6 +447,9 @@ static struct musb_platform_ops bfin_ops = {
 
 	.set_mode	= bfin_set_mode,
 	.try_idle	= bfin_try_idle,
+
+	.suspend	= bfin_suspend,
+	.resume		= bfin_resume,
 
 	.vbus_status	= bfin_vbus_status,
 	.set_vbus	= bfin_set_vbus,

FlopKernel-Series/flop_s5e8825_kernel

drivers/usb/musb/am35x.c
@@ -462,19 +462,19 @@ static int am35x_musb_exit(struct musb *musb)
 	return 0;
 }
 
-#ifdef CONFIG_PM
-void musb_platform_save_context(struct musb *musb,
-	struct musb_context_registers *musb_context)
+static int am35x_musb_suspend(struct musb *musb)
 {
 	phy_off();
+
+	return 0;
 }
 
-void musb_platform_restore_context(struct musb *musb,
-	struct musb_context_registers *musb_context)
+static int am35x_musb_resume(struct musb *musb)
 {
 	phy_on();
+
+	return 0;
 }
-#endif
 
 /* AM35x supports only 32bit read operation */
 void musb_read_fifo(struct musb_hw_ep *hw_ep, u16 len, u8 *dst)
@@ -515,6 +515,9 @@ static const struct musb_platform_ops am35x_ops = {
 
 	.set_mode	= am35x_musb_set_mode,
 	.try_idle	= am35x_musb_try_idle,
+
+	.suspend	= am35x_musb_suspend,
+	.resume		= am35x_musb_resume,
 
 	.set_vbus	= am35x_musb_set_vbus,
 };

drivers/usb/musb/blackfin.c
@@ -406,9 +406,7 @@ static int bfin_musb_init(struct musb *musb)
 	return 0;
 }
 
-#ifdef CONFIG_PM
-void musb_platform_save_context(struct musb *musb,
-			struct musb_context_registers *musb_context)
+static int bfin_musb_suspend(struct musb *musb)
 {
 	if (is_host_active(musb))
 		/*
@@ -418,14 +416,16 @@ void musb_platform_save_context(struct musb *musb,
 		 * wakeup event.
 		 */
 		gpio_set_value(musb->config->gpio_vrsel, 0);
+
+	return 0;
 }
 
-void musb_platform_restore_context(struct musb *musb,
-			struct musb_context_registers *musb_context)
+static int bfin_musb_resume(struct musb *musb)
 {
 	bfin_musb_reg_init(musb);
+
+	return 0;
 }
-#endif
 
 static int bfin_musb_exit(struct musb *musb)
 {
@@ -445,6 +445,9 @@ static const struct musb_platform_ops bfin_ops = {
 
 	.set_mode	= bfin_musb_set_mode,
 	.try_idle	= bfin_musb_try_idle,
+
+	.suspend	= bfin_musb_suspend,
+	.resume		= bfin_musb_resume,
 
 	.vbus_status	= bfin_musb_vbus_status,
 	.set_vbus	= bfin_musb_set_vbus,