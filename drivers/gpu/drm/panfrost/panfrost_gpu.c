e3fada28f77f40ec0ab8135c56d67cf0f5ff606a
Mary Guillemard <mary.guillemard@collabora.com>
2024-08-19 10:02:22 +0200

    BACKPORT: drm/panfrost: Add SYSTEM_TIMESTAMP and SYSTEM_TIMESTAMP_FREQUENCY parameters
    Expose system timestamp and frequency supported by the GPU.
    
    Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>
    (cherry picked from commit 9c75b16cabc6)
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    [Flopster101: Adapt to local panfrost_gpu implementation]

--- drivers/gpu/drm/panfrost/panfrost_gpu.c
+++ drivers/gpu/drm/panfrost/panfrost_gpu.c
@@ -410,6 +410,18 @@ unsigned long long panfrost_cycle_counter_read(struct panfrost_device *pfdev)
 	return ((u64)hi << 32) | lo;
 }
 
+unsigned long long panfrost_timestamp_read(struct panfrost_device *pfdev)
+{
+	u32 hi, lo;
+
+	do {
+		hi = gpu_read(pfdev, GPU_TIMESTAMP_HI);
+		lo = gpu_read(pfdev, GPU_TIMESTAMP_LO);
+	} while (hi != gpu_read(pfdev, GPU_TIMESTAMP_HI));
+
+	return ((u64)hi << 32) | lo;
+}
+
 void panfrost_gpu_power_on(struct panfrost_device *pfdev)
 {
 	int ret;
