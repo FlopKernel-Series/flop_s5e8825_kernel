BayLibre/android_kernel_common

ANDROID: vendor_hooks: add vendor hooks for fuse request
Add hooks to fuse queue request and request end so we can do boost
to those background tasks which block the UX related task.

Bug: 333220630
Change-Id: I9be59ed88675c5102c57ba9cbd26cf4df3d2fd7f
Signed-off-by: liliangliang <liliangliang@vivo.com>

include/trace/hooks/fuse.h
@@ -1,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM fuse
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH trace/hooks
+#if !defined(_TRACE_HOOK_FUSE_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_FUSE_H
+#include <trace/hooks/vendor_hooks.h>
+/*
+ * Following tracepoints are not exported in tracefs and provide a
+ * mechanism for vendor modules to hook and extend functionality
+ */
+
+struct fuse_iqueue;
+DECLARE_HOOK(android_vh_queue_request_and_unlock,
+	TP_PROTO(struct fuse_iqueue *fiq, bool sync),
+	TP_ARGS(fiq, sync));
+DECLARE_HOOK(android_vh_fuse_request_end,
+	TP_PROTO(struct task_struct *self),
+	TP_ARGS(self));
+
+#endif /* _TRACE_HOOK_FUSE_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>

FlopKernel-Series/flop_s5e8825_kernel

(cherry picked from commit e520c2932df0d1bbf83ae45c82ac01fd41655d77)
(cherry picked from commit e9fd05e64568bfb9fe1fb4c319d7be17ab1c448a)

include/trace/hooks/fuse.h
@@ -1,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM fuse
+#undef TRACE_INCLUDE_PATH
+#define TRACE_INCLUDE_PATH trace/hooks
+#if !defined(_TRACE_HOOK_FUSE_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_HOOK_FUSE_H
+#include <trace/hooks/vendor_hooks.h>
+/*
+ * Following tracepoints are not exported in tracefs and provide a
+ * mechanism for vendor modules to hook and extend functionality
+ */
+
+struct wait_queue_head;
+DECLARE_HOOK(android_vh_queue_request_and_unlock,
+	TP_PROTO(struct wait_queue_head *wq_head, bool sync),
+	TP_ARGS(wq_head, sync));
+DECLARE_HOOK(android_vh_fuse_request_end,
+	TP_PROTO(struct task_struct *self),
+	TP_ARGS(self));
+
+#endif /* _TRACE_HOOK_FUSE_H */
+/* This part must be outside protection */
+#include <trace/define_trace.h>