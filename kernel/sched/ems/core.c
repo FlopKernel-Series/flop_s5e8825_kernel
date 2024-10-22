46dd6048cb91075565e7a9e2c15eb8accb62e12e
Nahuel Gómez <nahuelgomez329@gmail.com>
2024-10-22 08:32:28 -0300

    kernel: sched: ems: drop usage of SCHED_FEAT
    We removed this.
    
    ../kernel/sched/ems/core.c:1370:23: error: use of undeclared identifier 'sched_feat_names'
     1370 |         index = match_string(sched_feat_names, __SCHED_FEAT_NR, "TTWU_QUEUE");
          |                              ^
    ../kernel/sched/ems/core.c:1370:41: error: use of undeclared identifier '__SCHED_FEAT_NR'
     1370 |         index = match_string(sched_feat_names, __SCHED_FEAT_NR, "TTWU_QUEUE");
          |                                                ^
    ../kernel/sched/ems/core.c:1372:23: error: use of undeclared identifier 'sched_feat_keys'
     1372 |                 static_key_disable(&sched_feat_keys[index]);
          |                                     ^
    ../kernel/sched/ems/core.c:1373:3: error: use of undeclared identifier 'sysctl_sched_features'; did you mean 'sysctl_sched_latency'?
     1373 |                 sysctl_sched_features &= ~(1UL << index);
          |                 ^~~~~~~~~~~~~~~~~~~~~
          |                 sysctl_sched_latency
    ../include/linux/sched/sysctl.h:29:21: note: 'sysctl_sched_latency' declared here
       29 | extern unsigned int sysctl_sched_latency;
          |                     ^
    4 errors generated.
    
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- kernel/sched/ems/core.c
+++ kernel/sched/ems/core.c
@@ -1354,7 +1354,6 @@ out:
 int core_init(struct device_node *ems_dn)
 {
 	struct device_node *dn, *child;
-	int index;
 
 	dn = of_find_node_by_name(ems_dn, "cpu-selection-domain");
 	if (unlikely(!dn))
@@ -1383,12 +1382,6 @@ int core_init(struct device_node *ems_dn)
 	tex_init();
 	cpus_binding_init();
 	cpu_weight_init();
-
-	index = match_string(sched_feat_names, __SCHED_FEAT_NR, "TTWU_QUEUE");
-	if (index >= 0) {
-		static_key_disable(&sched_feat_keys[index]);
-		sysctl_sched_features &= ~(1UL << index);
-	}
 
 	return 0;
 }
