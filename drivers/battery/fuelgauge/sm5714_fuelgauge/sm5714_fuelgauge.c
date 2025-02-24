ae40c1f60332f8cdfe5da9d86332742ddca635bd
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-02-24 19:47:31 -0300

    Revert "battery: sm5714_fuelgauge: Use CHARGE_FULL capacity for CHARGE_COUNTER"
    Causes charging issues on non-AOSP.
    
    This reverts commit c6763d3309745a01ce2a4877831cb470a73770e3.

--- drivers/battery/fuelgauge/sm5714_fuelgauge/sm5714_fuelgauge.c
+++ drivers/battery/fuelgauge/sm5714_fuelgauge/sm5714_fuelgauge.c
@@ -2104,8 +2104,8 @@ static int sm5714_fg_get_property(struct power_supply *psy,
 		val->intval = fuelgauge->capacity_max;
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_COUNTER:
-		psy_do_property("battery", get, POWER_SUPPLY_PROP_CHARGE_FULL, value);
-		val->intval = value.intval/1000 * fuelgauge->raw_capacity; //uAh
+		val->intval =
+			fuelgauge->battery_data->Capacity * fuelgauge->raw_capacity;
 		pr_info("%s: charge_counter = %d\n", __func__, val->intval);
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY_LEVEL:
