e7a1c158c05cba5064d8cae999b3650752f945dd
Flopster101 <nahuelgomez329@gmail.com>
2025-04-13 20:33:44 -0300

    FLOPPY: deps: Improve handling of dependencies
    Put them all into variables and don't ask for root access if the dependencies are already installed.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel_build/scripts/deps.sh
+++ kernel_build/scripts/deps.sh
@@ -7,20 +7,37 @@ else
     exit
 fi
 
-DEP_LIST="lz4 brotli flex bc cpio kmod ccache zip binutils-aarch64-linux-gnu"
+# Define dependency lists for each distro
+UBUNTU_DEPS="lz4 brotli flex bc cpio kmod ccache zip binutils-aarch64-linux-gnu"
+ARCH_DEPS="lz4 brotli flex bc cpio kmod ccache zip aarch64-linux-gnu-binutils"
+GENTOO_DEPS="app-arch/lz4 app-arch/brotli sys-devel/flex sys-devel/bc app-arch/cpio sys-apps/kmod dev-util/ccache app-arch/zip"
+COMMON_DEPS=$UBUNTU_DEPS
 
 if [ ! -f "$KDIR/kernel_build/.deps" ]; then
     if grep -q "Ubuntu" /etc/os-release; then
-        "$ROOT" apt update -qq
-        "$ROOT" apt install lz4 brotli flex bc cpio kmod ccache zip binutils-aarch64-linux-gnu -y
+        MISSING_DEPS=$(dpkg-query -W -f='${Status} ${Package}\n' $UBUNTU_DEPS 2>/dev/null | grep -v "install ok installed" | awk '{print $4}')
+        if [ -n "$MISSING_DEPS" ]; then
+            "$ROOT" apt update -qq
+            "$ROOT" apt install -y $MISSING_DEPS
+        fi
     elif grep -q "arch" "/etc/os-release"; then
-        "$ROOT" pacman -Syyuu --needed  --noconfirm lz4 brotli flex bc cpio kmod ccache zip aarch64-linux-gnu-binutils
+        MISSING_DEPS=$(pacman -T $ARCH_DEPS 2>/dev/null)
+        if [ -n "$MISSING_DEPS" ]; then
+            "$ROOT" pacman -Syyuu --needed --noconfirm $MISSING_DEPS
+        fi
     elif grep -q "gentoo" "/etc/os-release"; then
-        "$ROOT" emerge -navq app-arch/lz4 app-arch/brotli sys-devel/flex sys-devel/bc app-arch/cpio sys-apps/kmod dev-util/ccache app-arch/zip sys-devel/crossdev
-        "$ROOT" crossdev --target aarch64-linux-gnu
+        for dep in $GENTOO_DEPS; do
+            if ! equery list "$dep" >/dev/null 2>&1; then
+                "$ROOT" emerge -navq "$dep"
+            fi
+        done
+        if ! equery list crossdev >/dev/null 2>&1; then
+            "$ROOT" emerge -navq sys-devel/crossdev
+            "$ROOT" crossdev --target aarch64-linux-gnu
+        fi
     else
         echo -e "\nINFO: Your distro is not Supported, skipping dependencies installation..."
-        echo -e "INFO: Make sure you have these dependencies installed before proceeding: $UB_DEPLIST\n"
+        echo -e "INFO: Make sure you have these dependencies installed before proceeding: $COMMON_DEPS\n"
     fi
     touch "$KDIR/kernel_build/.deps"
 fi
