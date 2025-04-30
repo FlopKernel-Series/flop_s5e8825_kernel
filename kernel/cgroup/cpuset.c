a472d9a157a451ca6ff3d735154dd6c61c8a881d
Flopster101 <nahuelgomez329@gmail.com>
2025-04-30 15:32:23 -0300

    [SQUASH] kernel: cpuset: drop cpuset assist
    Can't tune it properly for now. May return in the future.
    
    * Reverted commits:
    
    Revert "kernel: cpuset: update cpusets for Cpuset Assist"
    
    This reverts commit 6f3157e2be9638f1607a4938cc4dc97aaedd6af1.
    
    Revert "kernel: cpuset: add a camera-background option to tune"
    
    This reverts commit 23ba245d7d8aeeb60dcd819efcf730cd4f4da743.
    
    Revert "kernel: cpuset: remove redundant task_is_booster redefinition"
    
    This reverts commit 54a09a028870d9ef073e9d661e5ceebc90fafd5a.
    
    Revert "kernel: cpuset: move cpuset configuration to Kconfig"
    
    This reverts commit dff2a53f0355fc229ae1ab9585ca576375b40471.
    
    Revert "kernel: cpuset: add audio-app cpuset"
    
    This reverts commit 5c6709cd79b04ccf9afc9846655d5bbb234ad4e1.
    
    Revert "kernel: fix build with CONFIG_CPUSET_ASSIST disabled"
    
    This reverts commit 95cf37905b018bee326230c5f7f2d288e7983272.
    
    Revert "cpuset: Refactor cpuset assist code"
    
    This reverts commit 485d0ff6faf042760a29f4fd6cbf7c38e0df1edc.
    
    Revert "kernel: introduce CPUSets Assist"
    
    This reverts commit ff14f3316d7108a6768433e324dbdb7e5b5a9d19.

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -69,7 +69,6 @@
 #include <linux/mutex.h>
 #include <linux/cgroup.h>
 #include <linux/wait.h>
-#include <linux/binfmts.h>
 
 #include <trace/hooks/sched.h>
 #include <trace/hooks/cgroup.h>
@@ -205,13 +204,6 @@ struct tmpmasks {
 	cpumask_var_t addmask, delmask;	/* For partition root */
 	cpumask_var_t new_cpus;		/* For update_cpumasks_hier() */
 };
-
-#ifdef CONFIG_CPUSET_ASSIST
-struct cs_target {
-	const char *name;
-	char *cpus;
-};
-#endif
 
 static inline struct cpuset *css_cs(struct cgroup_subsys_state *css)
 {
@@ -2501,6 +2493,8 @@ static ssize_t cpuset_write_resmask(struct kernfs_open_file *of,
 	struct cpuset *trialcs;
 	int retval = -ENODEV;
 
+	buf = strstrip(buf);
+
 	/*
 	 * CPU or memory hotunplug may leave @cs w/o any execution
 	 * resources, in which case the hotplug code asynchronously updates
@@ -2555,50 +2549,6 @@ out_unlock:
 	css_put(&cs->css);
 	flush_workqueue(cpuset_migrate_mm_wq);
 	return retval ?: nbytes;
-}
-
-#ifdef CONFIG_CPUSET_ASSIST
-static ssize_t cpuset_write_resmask_assist(struct kernfs_open_file *of,
-					   struct cs_target tgt, size_t nbytes,
-					   loff_t off)
-{
-	pr_info("cpuset_assist: setting %s to %s\n", tgt.name, tgt.cpus);
-	return cpuset_write_resmask(of, tgt.cpus, nbytes, off);
-}
-#endif
-
-static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
-					 char *buf, size_t nbytes, loff_t off)
-{
-#ifdef CONFIG_CPUSET_ASSIST
-	static struct cs_target cs_targets[] = {
-		{ "foreground",        CONFIG_CPUSET_FG },
-		{ "foreground_window", CONFIG_CPUSET_FOREGROUND_WINDOW },
-		{ "background",        CONFIG_CPUSET_BG },
-		{ "system-background", CONFIG_CPUSET_SYSTEM_BG },
-		{ "restricted",        CONFIG_CPUSET_RESTRICTED },
-		{ "top-app",           CONFIG_CPUSET_TOP_APP },
-		{ "camera-daemon",     CONFIG_CPUSET_CAMERA },
-		{ "dexopt",            CONFIG_CPUSET_DEXOPT },
-	};
-
-	struct cpuset *cs = css_cs(of_css(of));
-	int i;
-
-	if (task_is_booster(current)) {
-		for (i = 0; i < ARRAY_SIZE(cs_targets); i++) {
-			struct cs_target tgt = cs_targets[i];
-
-			if (!strcmp(cs->css.cgroup->kn->name, tgt.name))
-				return cpuset_write_resmask_assist(of, tgt,
-								   nbytes, off);
-		}
-	}
-#endif
-
-	buf = strstrip(buf);
-
-	return cpuset_write_resmask(of, buf, nbytes, off);
 }
 
 /*
@@ -2746,7 +2696,7 @@ static struct cftype legacy_files[] = {
 	{
 		.name = "cpus",
 		.seq_show = cpuset_common_seq_show,
-		.write = cpuset_write_resmask_wrapper,
+		.write = cpuset_write_resmask,
 		.max_write_len = (100U + 6 * NR_CPUS),
 		.private = FILE_CPULIST,
 	},
