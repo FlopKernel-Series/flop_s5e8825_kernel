6dfd4381a2cb3df64de0a73b575602cd2718560b
Adrián Larumbe <adrian.larumbe@collabora.com>
2025-05-20 18:43:59 +0100

    UPSTREAM: drm/panfrost: Internally label some BOs
    Perfcnt samples buffer is not exposed to UM, but keep a tag on it.
    
    Signed-off-by: Adrián Larumbe <adrian.larumbe@collabora.com>
    (cherry picked from commit ca8b3216dcea)
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/gpu/drm/panfrost/panfrost_perfcnt.c
+++ drivers/gpu/drm/panfrost/panfrost_perfcnt.c
@@ -109,6 +109,8 @@ static int panfrost_perfcnt_enable_locked(struct panfrost_device *pfdev,
 		goto err_put_mapping;
 	}
 
+	panfrost_gem_internal_set_label(&bo->base, "Perfcnt sample buffer");
+
 	/*
 	 * Invalidate the cache and clear the counters to start from a fresh
 	 * state.
