ced5fa1cf749a076b20d7bd0c5291ab6964fab4a
Flopster101 <nahuelgomez329@gmail.com>
2025-06-14 16:24:10 -0300

    include: workarounds: add header guards
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- include/linux/workarounds.h
+++ include/linux/workarounds.h
@@ -1,4 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _WORKAROUNDS_H
+#define _WORKAROUNDS_H
 
 #if defined(CONFIG_DEFAULT_SUPPORT_AOSP)
 static inline bool is_aosp_mode(void)
@@ -8,3 +10,5 @@ static inline bool is_aosp_mode(void)
 #else
 bool is_aosp_mode(void);
 #endif
+
+#endif /* _WORKAROUNDS_H */
