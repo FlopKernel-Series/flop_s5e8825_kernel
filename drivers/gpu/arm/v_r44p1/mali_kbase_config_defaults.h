36517ba653a21ca0f8a285d2ad392301993e69bf
Flopster101 <nahuelgomez329@gmail.com>
2025-06-18 14:05:55 -0300

    Revert "gpu: arm/r44p1: Adapt our changes for overclock"
    This reverts commit db31b473a765627fd7f1c69128b7ad5d6856c3d7.

--- drivers/gpu/arm/v_r44p1/mali_kbase_config_defaults.h
+++ drivers/gpu/arm/v_r44p1/mali_kbase_config_defaults.h
@@ -29,7 +29,6 @@
 
 /* Include mandatory definitions per platform */
 #include <mali_kbase_config_platform.h>
-#include <linux/exynos/s5e8825_clk_gpu.h>
 
 enum {
 	/* Use unrestricted Address ID width on the AXI bus. */
@@ -262,7 +261,7 @@ enum {
  * is enabled the value will be read from there, otherwise this should be
  * overridden by defining GPU_FREQ_KHZ_MAX in the platform file.
  */
-#define DEFAULT_GPU_FREQ_KHZ_MAX (GPU_FREQ_KHZ_MAX)
+#define DEFAULT_GPU_FREQ_KHZ_MAX (5000)
 
 /* Default timeout for task execution on an endpoint
  *
