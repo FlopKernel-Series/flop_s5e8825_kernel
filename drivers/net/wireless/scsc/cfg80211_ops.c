3f305aa8e04ea5c65abd0929b301775f55403750
Flopster101 <nahuelgomez329@gmail.com>
2025-07-06 23:30:29 -0300

    net: wireless/scsc: Guard off some more code with with is_aosp_mode()
    This corresponds to the changes from:
    9104766 ("drivers: net: wireless: scsc: Don't delete or deactivate AP interface in slsi_del_station")
    
    Some users were reporting issues with hotspot, so I'm doing this just to be safe.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/net/wireless/scsc/cfg80211_ops.c
+++ drivers/net/wireless/scsc/cfg80211_ops.c
@@ -20,6 +20,7 @@
 #include "log2us.h"
 #include "ba.h"
 #include <scsc/scsc_warn.h>
+#include <linux/workarounds.h>
 
 #ifdef CONFIG_SCSC_WLAN_ANDROID
 #include "scsc_wifilogger_rings.h"
@@ -173,9 +174,16 @@ int slsi_change_virtual_intf(struct wiphy *wiphy,
 
 	SLSI_NET_DBG1(dev, SLSI_CFG80211, "type:%u, iftype:%d\n", type, ndev_vif->iftype);
 
-	if (ndev_vif->vif_type != FAPI_VIFTYPE_AP && WARN_ON(ndev_vif->activated)) {
-		r = -EINVAL;
-		goto exit;
+	if (is_aosp_mode()) {
+		if (ndev_vif->vif_type != FAPI_VIFTYPE_AP && WARN_ON(ndev_vif->activated)) {
+			r = -EINVAL;
+			goto exit;
+		}
+	} else {
+		if (WARN_ON(ndev_vif->activated)) {
+			r = -EINVAL;
+			goto exit;
+		}
 	}
 
 	switch (type) {
@@ -1860,7 +1868,16 @@ int slsi_del_station(struct wiphy *wiphy, struct net_device *dev,
 		slsi_clear_cached_ies(&ndev_vif->ap.cache_wpa_ie, &ndev_vif->ap.wpa_ie_len);
 		slsi_clear_cached_ies(&ndev_vif->ap.cache_wmm_ie, &ndev_vif->ap.wmm_ie_len);
 
+		if (is_aosp_mode())
+			goto aosp_skip;
+
+		netif_carrier_off(dev);
+
 		/* All STA related packets and info should already have been flushed */
+		if (slsi_mlme_del_vif(sdev, dev) != 0)
+			SLSI_NET_ERR(dev, "slsi_mlme_del_vif failed\n");
+		slsi_vif_deactivated(sdev, dev);
+aosp_skip:
 		ndev_vif->ipaddress = cpu_to_be32(0);
 
 		if (ndev_vif->ap.p2p_gc_keys_set) {
