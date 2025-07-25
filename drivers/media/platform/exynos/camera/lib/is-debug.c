89e4aecd68ece8a0fe821cf82abd6c1ef4aa95b5
Flopster101 <nahuelgomez329@gmail.com>
2025-07-25 02:46:22 -0300

    Revert "media: exynos/camera: Don't panic on LIC overflow"
    This is pointless because, even if it doesn't panic by the camera driver,
    it will still freeze or end up causing a memory access violation crash.
    
    This reverts commit 4631010e09bcd6b573236a0c10a9ec54c92a0d56.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/media/platform/exynos/camera/lib/is-debug.c
+++ drivers/media/platform/exynos/camera/lib/is-debug.c
@@ -1288,14 +1288,10 @@ void is_debug_s2d(bool en_s2d, const char *fmt, ...)
 		err("[DBG] S2D!!!: %s", buf);
 		dump_stack();
 
-        //if (dbg_snapshot_expire_watchdog() < 0)
-        //    panic("DSS doesn't support S2D", buf);
-		return;
+		if (dbg_snapshot_expire_watchdog() < 0)
+			panic("DSS doesn't support S2D", buf);
 	} else {
-		err("[DBG] S2D!!!: %s", buf);
-		//panic(buf);
-		//dump_stack();
-		return;
+		panic(buf);
 	}
 }
 EXPORT_SYMBOL(is_debug_s2d);
