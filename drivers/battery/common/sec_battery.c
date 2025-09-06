00c063983f7ac20920718c89c12ccb3f32cbdd46
Flopster101 <nahuelgomez329@gmail.com>
2025-09-06 16:28:43 -0300

    Revert "sec_battery: Report charging current in milliamps (mA)"
    Breaks reading for AOSP.
    
    This reverts commit 0e4a5fc3d2e6db3b1c52cdc5da0c0917829c6bee.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/battery/common/sec_battery.c
+++ drivers/battery/common/sec_battery.c
@@ -7476,13 +7476,13 @@ static int sec_bat_get_property(struct power_supply *psy,
 		val->intval = battery->voltage_avg * 1000;
 		break;
 	case POWER_SUPPLY_PROP_CURRENT_NOW:
-		value.intval = SEC_BATTERY_CURRENT_MA;
+		value.intval = SEC_BATTERY_CURRENT_UA;
 		psy_do_property(battery->pdata->fuelgauge_name, get,
 			POWER_SUPPLY_PROP_CURRENT_NOW, value);
 		val->intval = value.intval;
 		break;
 	case POWER_SUPPLY_PROP_CURRENT_AVG:
-		value.intval = SEC_BATTERY_CURRENT_MA;
+		value.intval = SEC_BATTERY_CURRENT_UA;
 		psy_do_property(battery->pdata->fuelgauge_name, get,
 			POWER_SUPPLY_PROP_CURRENT_AVG, value);
 		val->intval = value.intval;
