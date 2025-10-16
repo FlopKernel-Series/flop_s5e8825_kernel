60f15827318448ad7f4f2e2e171ece7b0ac68e56
Flopster101 <nahuelgomez329@gmail.com>
2025-10-16 19:34:18 -0300

    include: sus_su: Fix header path for SukiSU
    We aren't using sus_su, but fix it anyway.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- include/linux/sus_su.h
+++ include/linux/sus_su.h
@@ -1,7 +1,11 @@
 #ifndef __KSU_H_SUS_SU
 #define __KSU_H_SUS_SU
 
+#if defined(CONFIG_KSU_NEXT)
 #include "../../drivers/kernelsu/kernel/core_hook.h"
+#elif defined(CONFIG_KSU_SUKI)
+#include "../../drivers/sukisu/kernel/core_hook.h"
+#endif
 
 int sus_su_fifo_init(int *maj_dev_num, char *drv_path);
 int sus_su_fifo_exit(int *maj_dev_num, char *drv_path);
