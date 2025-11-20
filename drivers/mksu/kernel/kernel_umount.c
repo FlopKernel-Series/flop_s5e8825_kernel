e876235218e8b15be3b4c502dfdd8ae43f50c6c1
Flopster101 <nahuelgomez329@gmail.com>
2025-11-20 18:50:24 -0300

    mksu: patch with susfs rev. a5e7449c
    susfs v2.0.0
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/mksu/kernel/kernel_umount.c
+++ drivers/mksu/kernel/kernel_umount.c
@@ -17,7 +17,11 @@
 #include "feature.h"
 #include "ksud.h"
 
+#ifndef CONFIG_KSU_SUSFS
 static bool ksu_kernel_umount_enabled = true;
+#else
+bool ksu_kernel_umount_enabled = true;
+#endif // #ifndef CONFIG_KSU_SUSFS
 
 static int kernel_umount_feature_get(u64 *value)
 {
@@ -50,7 +54,20 @@ static void ksu_umount_mnt(struct path *path, int flags)
     }
 }
 
+#ifdef CONFIG_KSU_SUSFS
+#if defined(CONFIG_KSU_SUSFS_TRY_UMOUNT) && defined(CONFIG_KSU_SUSFS_ENABLE_LOG)
+extern bool susfs_is_log_enabled;
+#endif // #if defined(CONFIG_KSU_SUSFS_TRY_UMOUNT) && defined(CONFIG_KSU_SUSFS_ENABLE_LOG)
+#ifdef CONFIG_KSU_SUSFS_TRY_UMOUNT
+extern void susfs_try_umount(void);
+#endif // #ifdef CONFIG_KSU_SUSFS_TRY_UMOUNT
+#endif // #ifdef CONFIG_KSU_SUSFS
+
+#ifndef CONFIG_KSU_SUSFS_TRY_UMOUNT
 static void try_umount(const char *mnt, int flags)
+#else
+void try_umount(const char *mnt, int flags)
+#endif // #ifndef CONFIG_KSU_SUSFS_TRY_UMOUNT
 {
     struct path path;
     int err = kern_path(mnt, 0, &path);
@@ -64,9 +81,22 @@ static void try_umount(const char *mnt, int flags)
         return;
     }
 
+#if defined(CONFIG_KSU_SUSFS_TRY_UMOUNT) && defined(CONFIG_KSU_SUSFS_ENABLE_LOG)
+    if (susfs_is_log_enabled) {
+        pr_info("susfs: umounting '%s'\n", mnt);
+    }
+#endif // #if defined(CONFIG_KSU_SUSFS_TRY_UMOUNT) && defined(CONFIG_KSU_SUSFS_ENABLE_LOG)
+
     ksu_umount_mnt(&path, flags);
 }
 
+#ifdef CONFIG_KSU_SUSFS_TRY_UMOUNT
+void susfs_try_umount_all(void) {
+    susfs_try_umount();
+}
+#endif // #ifdef CONFIG_KSU_SUSFS_TRY_UMOUNT
+
+#if !defined(CONFIG_KSU_SUSFS) || !defined(CONFIG_KSU_SUSFS_TRY_UMOUNT)
 struct umount_tw {
     struct callback_head cb;
     const struct cred *old_cred;
@@ -101,6 +131,7 @@ int ksu_handle_umount(uid_t old_uid, uid_t new_uid)
 {
     struct umount_tw *tw;
 
+#if defined(CONFIG_KSU_SUSFS) || !defined(CONFIG_KSU_SUSFS_TRY_UMOUNT)
     // this hook is used for umounting overlayfs for some uid, if there isn't any module mounted, just ignore it!
     if (!ksu_module_mounted) {
         return 0;
@@ -127,6 +158,7 @@ int ksu_handle_umount(uid_t old_uid, uid_t new_uid)
         pr_info("handle umount ignore non zygote child: %d\n", current->pid);
         return 0;
     }
+#endif // #if defined(CONFIG_KSU_SUSFS) || !defined(CONFIG_KSU_SUSFS_TRY_UMOUNT)
     // umount the target mnt
     pr_info("handle umount for uid: %d, pid: %d\n", new_uid, current->pid);
 
@@ -148,6 +180,7 @@ int ksu_handle_umount(uid_t old_uid, uid_t new_uid)
 
     return 0;
 }
+#endif // #if !defined(CONFIG_KSU_SUSFS) || !defined(CONFIG_KSU_SUSFS_TRY_UMOUNT)
 
 void ksu_kernel_umount_init(void)
 {
