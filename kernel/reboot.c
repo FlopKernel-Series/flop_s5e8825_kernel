6044a24e4f74c6d6d2eb973a92f6ca4390e63b47
Flopster101 <nahuelgomez329@gmail.com>
2025-11-20 19:34:12 -0300

    kernel: reboot: Fix KSU reboot hook for manual hooks & susfs
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -311,11 +311,11 @@ EXPORT_SYMBOL_GPL(kernel_power_off);
 
 DEFINE_MUTEX(system_transition_mutex);
 
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
 
-#ifdef CONFIG_KSU_SUSFS
+#if defined(CONFIG_KSU_SUSFS) && !defined(CONFIG_KSU_MANUAL_HOOK)
 extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
 /*
@@ -333,11 +333,11 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	char buffer[256];
 	int ret = 0;
 
-#ifdef CONFIG_KSU 
+#ifdef CONFIG_KSU_MANUAL_HOOK 
 	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
 #endif
 
-#ifdef CONFIG_KSU_SUSFS
+#if defined(CONFIG_KSU_SUSFS) && !defined(CONFIG_KSU_MANUAL_HOOK)
 	ret = ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
 	if (ret) {
 		goto orig_flow;
