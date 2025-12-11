f7efb02295a247284e17e01fccf200cccdcac801
Flopster101 <nahuelgomez329@gmail.com>
2025-12-11 11:43:32 -0300

    treewide: Patch with susfs rev. 1d426b8f
    gki-android12-5.10
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -315,6 +315,9 @@ DEFINE_MUTEX(system_transition_mutex);
 extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
 
+#ifdef CONFIG_KSU_SUSFS
+extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
+#endif
 /*
  * Reboot system call: for obvious reasons only root may call it,
  * and even root needs to set up some magic numbers in the registers
@@ -332,6 +335,15 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 
 #ifdef CONFIG_KSU_MANUAL_HOOK 
 	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+#endif
+
+#ifdef CONFIG_KSU_SUSFS
+	ret = ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+	if (ret) {
+		goto orig_flow;
+	}
+	return ret;
+orig_flow:
 #endif
 
 	/* We only trust the superuser with rebooting the system. */
