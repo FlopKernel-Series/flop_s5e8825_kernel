2a7f12de34d1744491846b6e5647917bb41bb75b
Flopster101 <nahuelgomez329@gmail.com>
2025-12-23 16:49:16 -0300

    FLOPPY: RIP RKSU
    Not gonna be supporting this one anymore either.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel_build/scripts/build.sh
+++ kernel_build/scripts/build.sh
@@ -22,7 +22,6 @@ build() {
     FRAGMENTS=""
     [ "$DO_KSU" = "1" ] && FRAGMENTS="$FRAGMENTS ksu.config"
     [ "$DO_SUKI" = "1" ] && FRAGMENTS="$FRAGMENTS sukisu.config"
-    [ "$DO_RKSU" = "1" ] && FRAGMENTS="$FRAGMENTS rksu.config"
 
     if [ "$DO_QUIET" = "1" ]; then
         make -j$(nproc --all) O=$OUTDIR CC="$CC" "$DEFCONFIG" $FRAGMENTS > /dev/null | tee log.txt
@@ -40,7 +39,7 @@ build() {
     rm -f "$OUT_KERNEL"
 
     if [ "$DO_REGEN" = "1" ]; then
-        if [ "$DO_KSU" = "1" ] || [ "$DO_SUKI" = "1" ] || [ "$DO_RKSU" = "1" ]; then
+        if [ "$DO_KSU" = "1" ] || [ "$DO_SUKI" = "1" ]; then
             echo "ERROR: Can't regenerate with KSU variant argument"
             exit 1
         fi
