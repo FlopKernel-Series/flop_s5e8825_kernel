4184e67c28bfb1f1517ff5f1c40c33ba2c0c1187
Flopster101 <nahuelgomez329@gmail.com>
2026-01-09 15:49:44 -0300

    Revert "block: zram: Set 4 GiB ZRAM for 6 GB devices"
    There are basically two choices for 6 GB devices:
    
    * Better multitasking with 4 GiB ZRAM, but performance that gets worse over
    time because Android is conservative about killing apps when it sees such
    big swap.
    
    * Better responsiveness and overall performance stability with 3 GiB ZRAM.
    
    I decided that the second one is probably a better choice by default for users.
    They can still change it to 4 GiB or whatever value if they choose to do so.
    
    This reverts commit b97b4fd3d10ec4c531f997ef9c067ff683e2c182.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -3493,8 +3493,8 @@ static ssize_t disksize_store(struct device *dev,
 			pr_info("Detected 8GB RAM variant (usable: %lu MB), setting ZRAM to 4GB",
 				total_ram_mb);
 		} else if (total_ram_mb > 4200) {
-			disksize = 4ULL * SZ_1G;
-			pr_info("Detected 6GB RAM variant (usable: %lu MB), setting ZRAM to 4GB",
+			disksize = 3ULL * SZ_1G;
+			pr_info("Detected 6GB RAM variant (usable: %lu MB), setting ZRAM to 3GB",
 				total_ram_mb);
 		} else {
 			disksize = 3ULL * SZ_1G;
