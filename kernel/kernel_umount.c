53210d56c2473aa3fb39445e586c390fffea398a
Flopster101 <nahuelgomez329@gmail.com>
2026-01-18 10:34:03 -0300

    Squashed 'drivers/kernelsu/' changes from be141d05d03f..511dfff82181
    511dfff82181 kernel: remove unnecesary duplicated code in sys_reboot (#1073)
    00127f2dee0d Revert "kernel: ksud: migrate init.rc handling to security_file_permission LSM" (#1056)
    d7de833a9c67 Sync legacy with dev branch and update scope minimized manual hooks 1.7 (#1047)
    0356464d9a4e kernel: fix ksu_handle_faccessat hook not found for manual hook check
    bab4d90a65e8 Merge pull request #1029 from maxsteeel/legacy
    1551e9a0eec2 kernel: set KSU_KPROBES_HOOK if not defined KSU_MANUAL_HOOK
    919b79a60835 kernel: apk_sign: fix return check for ksu_sha256
    8c0c055f93f1 kernel: supercalls: expose ksuver override
    5712ea72e663 kernel: Add some backports and remove some compatibility code
    d20831e4ebd1 kernel: remove unused cflags
    70830cb12271 kernel: update sulogv2
    90ce7c45f3db kernel: Explicitly check zygote start in execve hook
    69e9fa30cca5 kernel: Ensure manager is valid before installing fd (#3115)
    b68370790476 kernel: use selinux_cred() method instead of directly use cred->security (#3111)
    fd7bb77a4164 kernel: bumping fallback ksu_version  to  avoid bad integer value for userspace
    800a7a203071 sync legacy with dev branch and other things (#1021)
    a7a85040f13f fix issues in legacy branch (#1007)
    391eb9129e86 kernel: legacy, add missing early return in ksu_handle_execveat_sucompat (#1009)
    3d303237dcb8 kernel: correct header guard in seccomp_cache.h
    5f4c923db1d8 kernel: sync KSU_VERSION  with dev branch
    5abd0cbdcfff sync legacy branch with dev branch (#998)
    cfd00daefb84 kernel: fix compilation errors in non-gki kernels (#996)
    4f8090cdc273 fix some issues in legacy branch (#995)
    a93da680ebad add support for legacy/non-gki kernels (#976)
    
    git-subtree-dir: drivers/kernelsu
    git-subtree-split: 511dfff8218188c412ae758c29e171f0d22889af

--- kernel/kernel_umount.c
+++ kernel/kernel_umount.c
@@ -9,6 +9,9 @@
 #include <linux/path.h>
 #include <linux/printk.h>
 #include <linux/types.h>
+#ifndef KSU_HAS_PATH_UMOUNT
+#include <linux/syscalls.h>
+#endif
 
 #include "kernel_umount.h"
 #include "klog.h" // IWYU pragma: keep
@@ -16,6 +19,8 @@
 #include "selinux/selinux.h"
 #include "feature.h"
 #include "ksud.h"
+#include "ksu.h"
+#include "kernel_compat.h"
 
 static bool ksu_kernel_umount_enabled = true;
 
@@ -40,8 +45,9 @@ static const struct ksu_feature_handler kernel_umount_handler = {
 	.set_handler = kernel_umount_feature_set,
 };
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) ||                           \
+	defined(KSU_HAS_PATH_UMOUNT)
 extern int path_umount(struct path *path, int flags);
-
 static void ksu_umount_mnt(struct path *path, int flags)
 {
 	int err = path_umount(path, flags);
@@ -49,6 +55,29 @@ static void ksu_umount_mnt(struct path *path, int flags)
 		pr_info("umount %s failed: %d\n", path->dentry->d_iname, err);
 	}
 }
+#else
+static void ksu_sys_umount(const char *mnt, int flags)
+{
+	char __user *usermnt = (char __user *)mnt;
+	mm_segment_t old_fs;
+
+	old_fs = get_fs();
+	set_fs(KERNEL_DS);
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
+	ksys_umount(usermnt, flags);
+#else
+	sys_umount(usermnt, flags); // cuz asmlinkage long sys##name
+#endif
+	set_fs(old_fs);
+}
+
+#define ksu_umount_mnt(mnt, __unused, flags)                                   \
+	({                                                                     \
+		path_put(__unused);                                            \
+		ksu_sys_umount(mnt, flags);                                    \
+	})
+
+#endif
 
 static void try_umount(const char *mnt, int flags)
 {
@@ -63,22 +92,21 @@ static void try_umount(const char *mnt, int flags)
 		path_put(&path);
 		return;
 	}
-
-    ksu_umount_mnt(&path, flags);
+#ifndef KSU_HAS_PATH_UMOUNT
+    ksu_umount_mnt(mnt, &path, flags);
+#else
+	ksu_umount_mnt(&path, flags);
+#endif
 }
 
 struct umount_tw {
 	struct callback_head cb;
-	const struct cred *old_cred;
 };
 
 static void umount_tw_func(struct callback_head *cb)
 {
 	struct umount_tw *tw = container_of(cb, struct umount_tw, cb);
-	const struct cred *saved = NULL;
-	if (tw->old_cred) {
-		saved = override_creds(tw->old_cred);
-	}
+	const struct cred *saved = override_creds(ksu_cred);
 
     struct mount_entry *entry;
     down_read(&mount_list_lock);
@@ -88,11 +116,7 @@ static void umount_tw_func(struct callback_head *cb)
     }
     up_read(&mount_list_lock);
 
-	if (saved)
-		revert_creds(saved);
-
-	if (tw->old_cred)
-		put_cred(tw->old_cred);
+	revert_creds(saved);
 
 	kfree(tw);
 }
@@ -107,6 +131,10 @@ int ksu_handle_umount(uid_t old_uid, uid_t new_uid)
 	}
 
 	if (!ksu_kernel_umount_enabled) {
+		return 0;
+	}
+
+	if (!ksu_cred) {
 		return 0;
 	}
 
@@ -140,14 +168,10 @@ int ksu_handle_umount(uid_t old_uid, uid_t new_uid)
 	if (!tw)
 		return 0;
 
-	tw->old_cred = get_current_cred();
 	tw->cb.func = umount_tw_func;
 
 	int err = task_work_add(current, &tw->cb, TWA_RESUME);
 	if (err) {
-		if (tw->old_cred) {
-			put_cred(tw->old_cred);
-		}
 		kfree(tw);
 		pr_warn("unmount add task_work failed\n");
 	}
