76558108934e9e582d4e82d5f4c9eaad9fe10c8c
Flopster101 <nahuelgomez329@gmail.com>
2026-01-19 12:22:42 -0300

    [DNM] fingerprint: el7xx: Instrument driver to log SPI data transfers
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/fingerprint/el7xx-spi_data_transfer.c
+++ drivers/fingerprint/el7xx-spi_data_transfer.c
@@ -56,10 +56,30 @@ int el7xx_spi_sync(struct el7xx_data *etspi, int len)
 
 	spi_message_init(&m);
 	spi_message_add_tail(&xfer, &m);
+
+	/* brief INFO log for every SPI transfer to correlate with userspace ioctl */
+	if (len > 0)
+		pr_info("el7xx SPI TX pid=%d len=%u op=0x%02x b1=0x%02x b2=0x%02x b3=0x%02x\n",
+			current->pid, (unsigned int)len, (unsigned int)etspi->buf[0],
+			(unsigned int)((len > 1) ? etspi->buf[1] : 0x00),
+			(unsigned int)((len > 2) ? etspi->buf[2] : 0x00),
+			(unsigned int)((len > 3) ? etspi->buf[3] : 0x00));
+	else
+		pr_info("el7xx SPI TX pid=%d len=0\n", current->pid);
+
 	retval = spi_sync(etspi->spi, &m);
 
-	if (retval < 0)
-		pr_info("error retval = %d\n", retval);
+	if (retval < 0) {
+		pr_info("el7xx SPI RX pid=%d len=%u op=0x%02x ret=%d\n",
+			current->pid, (unsigned int)len, (unsigned int)((len > 0) ? etspi->buf[0] : 0xff), retval);
+		pr_err("error retval = %d\n", retval);
+	} else {
+		pr_info("el7xx SPI RX pid=%d len=%u op=0x%02x b1=0x%02x b2=0x%02x b3=0x%02x\n",
+			current->pid, (unsigned int)len, (unsigned int)etspi->buf[0],
+			(unsigned int)((len > 1) ? etspi->buf[1] : 0x00),
+			(unsigned int)((len > 2) ? etspi->buf[2] : 0x00),
+			(unsigned int)((len > 3) ? etspi->buf[3] : 0x00));
+	}
 
 	return retval;
 }
@@ -84,7 +104,8 @@ int el7xx_io_read_register(struct el7xx_data *etspi, u8 *addr, u8 *buf, struct e
 		return retval;
 	}
 
-	pr_debug("len = %d addr = %p val = %x\n", read_len, addr, etspi->buf[2]);
+	pr_info("el7xx REG-R pid=%d addr=0x%02x val=0x%02x\n",
+		current->pid, (unsigned int)etspi->buf[1], (unsigned int)etspi->buf[2]);
 
 	if (copy_to_user((u8 __user *) (uintptr_t) buf, etspi->buf + 2, read_len)) {
 		pr_err("buffer copy_to_user fail retval\n");
@@ -113,12 +134,18 @@ int el7xx_io_burst_read_register(struct el7xx_data *etspi, struct egis_ioc_trans
 	}
 	pr_debug("tx_buf = %p op = %x reg = %x, len = %d\n",
 			ioc->tx_buf, *etspi->buf, *(etspi->buf + 1), spi_len);
+	pr_info("el7xx BURST-R TX pid=%d op=0x%02x reg=0x%02x len=%u\n",
+		current->pid, (unsigned int)etspi->buf[0], (unsigned int)etspi->buf[1], (unsigned int)spi_len);
 
 	retval = el7xx_spi_sync(etspi, spi_len);
 	if (retval < 0) {
 		pr_err("error retval = %d\n", retval);
 		return retval;
 	}
+
+	pr_info("el7xx BURST-R RX pid=%d op=0x%02x reg=0x%02x len=%u b1=0x%02x\n",
+		current->pid, (unsigned int)etspi->buf[0], (unsigned int)etspi->buf[1], (unsigned int)ioc->len,
+		(unsigned int)etspi->buf[2]);
 
 	if (copy_to_user((u8 __user *) (uintptr_t)ioc->rx_buf, etspi->buf + 2,
 				ioc->len)) {
@@ -148,12 +175,18 @@ int el7xx_io_burst_read_register_backward(struct el7xx_data *etspi, struct egis_
 	}
 	pr_debug("tx_buf = %p op = %x reg = %x, len = %d\n",
 			ioc->tx_buf, *etspi->buf, *(etspi->buf + 1), spi_len);
+	pr_info("el7xx BURST-R TX pid=%d op=0x%02x reg=0x%02x len=%u\n",
+		current->pid, (unsigned int)etspi->buf[0], (unsigned int)etspi->buf[1], (unsigned int)spi_len);
 
 	retval = el7xx_spi_sync(etspi, spi_len);
 	if (retval < 0) {
 		pr_err("error retval = %d\n", retval);
 		return retval;
 	}
+
+	pr_info("el7xx BURST-R RX pid=%d op=0x%02x reg=0x%02x len=%u b1=0x%02x\n",
+		current->pid, (unsigned int)etspi->buf[0], (unsigned int)etspi->buf[1], (unsigned int)ioc->len,
+		(unsigned int)etspi->buf[2]);
 
 	if (copy_to_user((u8 __user *) (uintptr_t)ioc->rx_buf, etspi->buf + 2,
 			ioc->len)) {
@@ -182,6 +215,8 @@ int el7xx_io_write_register(struct el7xx_data *etspi, u8 *buf, struct egis_ioc_t
 	etspi->buf[3] = etspi->buf[2];
 	pr_debug("write_len = %d addr = %x data = %x\n",
 			write_len, etspi->buf[1], etspi->buf[2]);
+	pr_info("el7xx REG-W TX pid=%d addr=0x%02x data=0x%02x\n",
+		current->pid, (unsigned int)etspi->buf[1], (unsigned int)etspi->buf[2]);
 
 	retval = el7xx_spi_sync(etspi, spi_len);
 	if (retval < 0) {
@@ -259,14 +294,18 @@ int el7xx_io_read_efuse(struct el7xx_data *etspi, struct egis_ioc_transfer *ioc)
 	memset(etspi->buf, 0, spi_len);
 	etspi->buf[0] = OP_EF_R;
 
-	pr_debug("len = %d, xfer.len = %d, buf = %p, rx_buf = %p\n",
-			ioc->len, spi_len, etspi->buf, ioc->rx_buf);
+	pr_info("el7xx EFUSE-R TX pid=%d len=%u spi_len=%d op=0x%02x\n",
+		current->pid, (unsigned int)ioc->len, spi_len, (unsigned int)etspi->buf[0]);
 
 	retval = el7xx_spi_sync(etspi, spi_len);
 	if (retval < 0) {
 		pr_err("read data error retval = %d\n", retval);
 		return retval;
 	}
+
+	pr_info("el7xx EFUSE-R RX pid=%d len=%u ret=%d b1=0x%02x\n",
+		current->pid, (unsigned int)ioc->len, retval,
+		(unsigned int)etspi->buf[1]);
 
 	if (copy_to_user((u8 __user *) (uintptr_t) ioc->rx_buf, etspi->buf + 1,
 			ioc->len)) {
@@ -290,8 +329,8 @@ int el7xx_io_write_efuse(struct el7xx_data *etspi, struct egis_ioc_transfer *ioc
 		return -EFAULT;
 	}
 
-	pr_debug("len = %d, xfer.len = %d, buf = %p, tx_buf = %p\n",
-			 ioc->len, spi_len, etspi->buf, ioc->tx_buf);
+	pr_info("el7xx EFUSE-W TX pid=%d len=%u spi_len=%d op=0x%02x\n",
+		current->pid, (unsigned int)ioc->len, spi_len, (unsigned int)etspi->buf[0]);
 
 	retval = el7xx_spi_sync(etspi, spi_len);
 	if (retval < 0)
@@ -308,6 +347,8 @@ int el7xx_io_get_frame(struct el7xx_data *etspi, u8 *fr, u32 size)
 	memset(etspi->buf, 0, spi_len);
 	etspi->buf[0] = OP_FB_R;
 
+	pr_info("el7xx IMG-R TX pid=%d req_size=%u spi_len=%d op=0x%02x\n",
+		current->pid, (unsigned int)size, spi_len, (unsigned int)etspi->buf[0]);
 	pr_debug("size = %d, xfer.len = %d, buf = %p, fr = %p\n",
 		size, spi_len, etspi->buf, fr);
 
@@ -316,6 +357,11 @@ int el7xx_io_get_frame(struct el7xx_data *etspi, u8 *fr, u32 size)
 		pr_err("read data error retval = %d\n", retval);
 		return retval;
 	}
+
+	pr_info("el7xx IMG-R RX pid=%d req_size=%u ret=%d first=0x%02x second=0x%02x\n",
+		current->pid, (unsigned int)size, retval,
+		(etspi->buf && spi_len > 1) ? etspi->buf[1] : 0x00,
+		(spi_len > 2) ? etspi->buf[2] : 0x00);
 
 	if (copy_to_user((u8 __user *) (uintptr_t) fr, etspi->buf + 1, size)) {
 		pr_err("buffer copy_to_user fail retval\n");
@@ -337,6 +383,8 @@ int el7xx_io_write_frame(struct el7xx_data *etspi, u8 *fr, u32 size)
 		return -EFAULT;
 	}
 
+	pr_info("el7xx IMG-W TX pid=%d size=%u spi_len=%d op=0x%02x\n",
+		current->pid, (unsigned int)size, spi_len, (unsigned int)etspi->buf[0]);
 	pr_debug("size = %d, xfer.len = %d, buf = %p, fr = %p\n",
 		size, spi_len, etspi->buf, fr);
 
@@ -358,11 +406,16 @@ int el7xx_io_transfer_command(struct el7xx_data *etspi, u8 *tx, u8 *rx, u32 size
 		return -EFAULT;
 	}
 
+	pr_info("el7xx TRANSFER-CMD TX pid=%d size=%u op=0x%02x\n",
+		current->pid, (unsigned int)size, (unsigned int)etspi->buf[0]);
 	retval = el7xx_spi_sync(etspi, size);
 	if (retval < 0) {
 		pr_err("retval = %d\n", retval);
 		return retval;
 	}
+	pr_info("el7xx TRANSFER-CMD RX pid=%d size=%u ret=%d b1=0x%02x\n",
+		current->pid, (unsigned int)size, retval,
+		(etspi->buf && size > 1) ? etspi->buf[1] : 0x00);
 
 	if (copy_to_user(rx, (const u8 __user *)(uintptr_t)etspi->buf, size)) {
 		pr_err("buffer copy_to_user fail. tr(%p) rx(%p)\n", etspi->buf,
