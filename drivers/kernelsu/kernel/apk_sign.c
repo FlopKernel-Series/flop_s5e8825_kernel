8c780b00d017a7dcdbea5ac06b7c612dcf4a3503
Flopster101 <nahuelgomez329@gmail.com>
2026-03-10 19:35:25 -0300

    kernelsu-next: Support KowSU manager
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/kernelsu/kernel/apk_sign.c
+++ drivers/kernelsu/kernel/apk_sign.c
@@ -18,6 +18,16 @@
 #include "app_profile.h"
 #include "klog.h" // IWYU pragma: keep
 #include "kernel_compat.h"
+#include "manager_sign.h"
+
+static const apk_sign_key_t apk_sign_keys[] = {
+	{ EXPECTED_SIZE_NEXT, EXPECTED_HASH_NEXT },
+	{ EXPECTED_SIZE_KOWX712, EXPECTED_HASH_KOWX712 },
+#ifdef EXPECTED_MANAGER_SIZE
+	{ EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH },
+#endif
+	{ 0, NULL }
+};
 
 struct sdesc {
 	struct shash_desc shash;
@@ -72,7 +82,7 @@ static int ksu_sha256(const unsigned char *data, unsigned int datalen,
 }
 
 static bool check_block(struct file *fp, u32 *size4, loff_t *pos, u32 *offset,
-			unsigned expected_size, const char *expected_sha256)
+			const apk_sign_key_t *keys)
 {
 	ksu_kernel_read_compat(fp, size4, 0x4, pos); // signer-sequence length
 	ksu_kernel_read_compat(fp, size4, 0x4, pos); // signer length
@@ -88,33 +98,35 @@ static bool check_block(struct file *fp, u32 *size4, loff_t *pos, u32 *offset,
 	ksu_kernel_read_compat(fp, size4, 0x4, pos); // certificates length
 	ksu_kernel_read_compat(fp, size4, 0x4, pos); // certificate length
 	*offset += 0x4 * 2;
-
-	if (*size4 == expected_size) {
-		*offset += *size4;
+	*offset += *size4;
 
 #define CERT_MAX_LENGTH 1024
-		char cert[CERT_MAX_LENGTH];
-		if (*size4 > CERT_MAX_LENGTH) {
-			pr_info("cert length overlimit\n");
-			return false;
-		}
-		ksu_kernel_read_compat(fp, cert, *size4, pos);
-		unsigned char digest[SHA256_DIGEST_SIZE];
-		if (ksu_sha256(cert, *size4, digest) < 0 ) {
-			pr_info("sha256 error\n");
-			return false;
-		}
+	char cert[CERT_MAX_LENGTH];
+	unsigned char digest[SHA256_DIGEST_SIZE];
+	char hash_str[SHA256_DIGEST_SIZE * 2 + 1];
+	const apk_sign_key_t *key;
 
-		char hash_str[SHA256_DIGEST_SIZE * 2 + 1];
-		hash_str[SHA256_DIGEST_SIZE * 2] = '\0';
-
-		bin2hex(hash_str, digest, SHA256_DIGEST_SIZE);
-		pr_info("sha256: %s, expected: %s\n", hash_str,
-			expected_sha256);
-		if (strcmp(expected_sha256, hash_str) == 0) {
-			return true;
-		}
+	if (*size4 > CERT_MAX_LENGTH) {
+		pr_info("cert length overlimit\n");
+		return false;
 	}
+
+	if (ksu_kernel_read_compat(fp, cert, *size4, pos) != *size4)
+		return false;
+
+	if (ksu_sha256(cert, *size4, digest) < 0) {
+		pr_info("sha256 error\n");
+		return false;
+	}
+
+	hash_str[SHA256_DIGEST_SIZE * 2] = '\0';
+	bin2hex(hash_str, digest, SHA256_DIGEST_SIZE);
+
+	for (key = keys; key->size != 0; key++) {
+		if (*size4 == key->size && strcmp(key->sha256, hash_str) == 0)
+			return true;
+	}
+
 	return false;
 }
 
@@ -172,8 +184,7 @@ static bool has_v1_signature_file(struct file *fp)
 }
 
 static __always_inline bool check_v2_signature(char *path,
-						unsigned expected_size,
-						const char *expected_sha256)
+						const apk_sign_key_t *keys)
 {
 	unsigned char buffer[0x11] = { 0 };
 	u32 size4;
@@ -245,8 +256,7 @@ static __always_inline bool check_v2_signature(char *path,
 		if (id == 0x7109871au) {
 			v2_signing_blocks++;
 			v2_signing_valid =
-				check_block(fp, &size4, &pos, &offset,
-						expected_size, expected_sha256);
+				check_block(fp, &size4, &pos, &offset, keys);
 		} else if (id == 0xf05368c0u) {
 			// http://aospxref.com/android-14.0.0_r2/xref/frameworks/base/core/java/android/util/apk/ApkSignatureSchemeV3Verifier.java#73
 			v3_signing_exist = true;
@@ -367,5 +377,5 @@ bool is_manager_apk(char *path)
 		return false;
 	}
 #endif
-	return check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH);
+	return check_v2_signature(path, apk_sign_keys);
 }
