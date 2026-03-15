5a36f170f993258f4b01add2680b13cfab3d63a9
Flopster101 <nahuelgomez329@gmail.com>
2026-03-15 02:46:23 -0300

    FLOPPY: Remove NOSUS label from KSU Next
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel_build/ckbuild.sh
+++ kernel_build/ckbuild.sh
@@ -203,7 +203,7 @@ fi
 LINUX_VER=$(make kernelversion 2>/dev/null)
 
 if [ "$DO_KSU" == "1" ]; then
-    FK_TYPE="KSUNext-NOSUS"
+    FK_TYPE="KSUNext"
     FK_TYPE_SHORT="KN"
 elif [ "$DO_SUKI" == "1" ]; then
     FK_TYPE="ReSukiSU"
