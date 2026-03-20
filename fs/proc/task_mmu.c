81d50f32cc158b0a2f435422a0042d9068c58231
simonpunk <simonpunk2016@gmail.com>
2026-03-20 10:46:06 +0800

    kernel: Rename some function names for sus_kstat to avoid naming confusion
    - Since both sus_map and sus_kstat spoof for show_map_vma, therefore it is better to use different name to tell which feature is doing for its own part.

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -482,7 +482,7 @@ static void show_vma_header_prefix_fake(struct seq_file *m,
 }
 
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-extern void susfs_show_map_vma_spoofer(struct inode *inode, dev_t *out_dev, unsigned long *out_ino);
+extern void susfs_sus_kstat_spoof_show_map_vma(struct inode *inode, dev_t *out_dev, unsigned long *out_ino);
 #endif // #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 extern int susfs_open_redirect_spoof_show_map_vma(struct inode *inode, unsigned long *out_ino, dev_t *out_dev, char *spoofed_name);
@@ -538,7 +538,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 		dev = inode->i_sb->s_dev;
 		ino = inode->i_ino;
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-		susfs_show_map_vma_spoofer(inode, &dev, &ino);
+		susfs_sus_kstat_spoof_show_map_vma(inode, &dev, &ino);
 #endif
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
 		dentry = file->f_path.dentry;
