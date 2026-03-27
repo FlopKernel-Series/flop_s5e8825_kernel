83a7252449b91eb319c08703634cb2356ea50770
Flopster101 <nahuelgomez329@gmail.com>
2026-03-27 15:25:31 -0300

    FLOPPY: ckbuild: Specify which variants have susfs
    Instead of it being the other way around.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel_build/ckbuild.sh
+++ kernel_build/ckbuild.sh
@@ -206,10 +206,10 @@ if [ "$DO_KSU" == "1" ]; then
     FK_TYPE="KSUNext"
     FK_TYPE_SHORT="KN"
 elif [ "$DO_SUKI" == "1" ]; then
-    FK_TYPE="ReSukiSU"
+    FK_TYPE="ReSukiSU-SUSFS"
     FK_TYPE_SHORT="RESKS"
 elif [ "$DO_MAMBO" == "1" ]; then
-    FK_TYPE="MamboSU"
+    FK_TYPE="MamboSU-SUSFS"
     FK_TYPE_SHORT="MBS"
 else
     FK_TYPE="Vanilla"
