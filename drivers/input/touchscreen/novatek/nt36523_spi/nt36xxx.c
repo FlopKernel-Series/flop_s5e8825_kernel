a4a10591ef976213220dfc8cf0d365accb2641c5
Flopster101 <nahuelgomez329@gmail.com>
2026-03-28 16:32:14 -0300

    Revert "input: touchscreen(fts, gdx, nvt): Snap proximity value to 5cm"
    This reverts commit ee7c1b81e4348c8d2808fdb7c3eded8eaa831e57.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/input/touchscreen/novatek/nt36523_spi/nt36xxx.c
+++ drivers/input/touchscreen/novatek/nt36523_spi/nt36xxx.c
@@ -2402,7 +2402,7 @@ void nvt_ts_proximity_report(uint8_t *data)
 	status = p_event_proximity->status;
 
 	if (is_aosp_mode_fast())
-		status = !status ? 5 : 0;
+		status = !status;
 
 	input_info(true, &ts->client->dev,"proximity->status = %d\n", status);
 
