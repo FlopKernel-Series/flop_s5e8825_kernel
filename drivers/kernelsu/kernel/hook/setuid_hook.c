e5dc6180850e105dc598d047dd4ccf79e82b12d6
Flopster101 <nahuelgomez329@gmail.com>
2026-05-10 02:48:13 -0300

    kernelsu-next: schedule SUSFS extra work
    Align with newer SUSFS changes.
    
    ld.lld: error: undefined symbol: susfs_run_sus_path_loop
    >>> referenced by setuid_hook.c:65 (../drivers/kernelsu/kernel/hook/setuid_hook.c:65)
    >>>               vmlinux.o:(susfs_handle_setuid_tw_func)
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/kernelsu/kernel/hook/setuid_hook.c
+++ drivers/kernelsu/kernel/hook/setuid_hook.c
@@ -15,6 +15,7 @@
 #include <linux/uaccess.h>
 #include <linux/uidgid.h>
 #include <linux/version.h>
+#include <linux/workqueue.h>
 
 #include "policy/allowlist.h"
 #include "setuid_hook.h"
@@ -46,45 +47,11 @@ static inline bool is_zygote_normal_app_uid(uid_t uid)
 }
 
 extern u32 susfs_zygote_sid;
-extern struct cred *ksu_cred;
-
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-extern void susfs_run_sus_path_loop(void);
-#endif // #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-
-struct susfs_handle_setuid_tw {
-    struct callback_head cb;
-};
-
-static void susfs_handle_setuid_tw_func(struct callback_head *cb)
-{
-    struct susfs_handle_setuid_tw *tw = container_of(cb, struct susfs_handle_setuid_tw, cb);
-    const struct cred *saved = override_creds(ksu_cred);
-
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-    susfs_run_sus_path_loop();
-#endif // #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-
-    revert_creds(saved);
-    kfree(tw);
-}
+extern struct work_struct susfs_extra_works;
 
 static void ksu_handle_extra_susfs_work(void)
 {
-    struct susfs_handle_setuid_tw *tw = kzalloc(sizeof(*tw), GFP_ATOMIC);
-
-    if (!tw) {
-        pr_err("susfs: No enough memory\n");
-        return;
-    }
-
-    tw->cb.func = susfs_handle_setuid_tw_func;
-
-    int err = task_work_add(current, &tw->cb, TWA_RESUME);
-    if (err) {
-        kfree(tw);
-        pr_err("susfs: Failed adding task_work 'susfs_handle_setuid_tw', err: %d\n", err);
-    }
+    schedule_work(&susfs_extra_works);
 }
 #ifdef CONFIG_KSU_SUSFS_TRY_UMOUNT
 extern void susfs_try_umount(uid_t uid);
@@ -178,10 +145,6 @@ do_umount:
 #else
     susfs_try_umount(new_uid);
 #endif // #ifndef CONFIG_KSU_SUSFS_TRY_UMOUNT
-
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-    //susfs_run_sus_path_loop(new_uid);
-#endif // #ifdef CONFIG_KSU_SUSFS_SUS_PATH
 
     ksu_handle_extra_susfs_work();
 
