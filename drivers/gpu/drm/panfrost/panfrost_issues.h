79b6b3f6023a30bc19ae2a3916fcc48d95758c5f
Flopster101 <nahuelgomez329@gmail.com>
2026-09-08 18:44:18 -0300

    drm/panfrost: Add Mali-G68 GPU support
    Add hardware features, issues, and GPU ID definition for Mali-G68 (Valhall v7 / JM architecture, product ID 0x9004), used in Samsung Exynos 1280 (s5e8825).
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- drivers/gpu/drm/panfrost/panfrost_issues.h
+++ drivers/gpu/drm/panfrost/panfrost_issues.h
@@ -265,6 +265,14 @@ enum panfrost_hw_issue {
 #define hw_issues_g57_r0p0 (\
 	BIT_ULL(HW_ISSUE_TTRX_3485))
 
+#define hw_issues_g68 hw_issues_g57
+
+#define hw_issues_g68_r0p0 (\
+	BIT_ULL(HW_ISSUE_TTRX_3485))
+
+#define hw_issues_g68_r1p0 (\
+	BIT_ULL(HW_ISSUE_TTRX_3485))
+
 static inline bool panfrost_has_hw_issue(const struct panfrost_device *pfdev,
 					 enum panfrost_hw_issue issue)
 {
